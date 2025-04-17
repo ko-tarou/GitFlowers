80cb36133419a891e61d3890afb79871aa1e2570
ko-tarou <tkota6174@gmail.com>
2025-04-17 11:40:18 +0900

    add Textの表示 ・変数の表示 ・FontSizeの変更 ・色の変更 ・カラーコード指定のためのメソッド作成

--- GitFlowers/Screen/MainScreen.swift
+++ GitFlowers/Screen/MainScreen.swift
@@ -1,7 +1,29 @@
-//
-//  MainScreen.swift
-//  GitFlowers
-//
-//  Created by kota on 2025/04/15.
-//
 
+import SwiftUI
+
+struct MainView: View {
+    
+    var CommitCount : Int = 0
+    
+    var body : some View {
+        Text("\(CommitCount)")
+            .font(.system(size: 100))
+            .foregroundStyle(Color(UIColor(hex: "229B00")))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+    }
+}
